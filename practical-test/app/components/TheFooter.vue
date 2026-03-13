6f93603473000fdeaa63b535aca9fd671366feb2
yesenia180405 <yes.ale1804@gmail.com>
2026-03-13 12:00:36 +0100

    feat(components/TheFooter.vue): creation a component The Footer

--- practical-test/app/components/TheFooter.vue
+++ practical-test/app/components/TheFooter.vue
@@ -1,6 +1,6 @@
 <script setup></script>
 <template>
-  <footer class="bg-footer-b text-white w-full py-4 text-sm">
+  <footer class="bg-footer-b text-footer-text w-full py-4 tracking-wide">
     <section class="grid grid-cols-3 items-center mr-28 ml-28">
       <img
         src="https://images.vexels.com/media/users/3/135535/isolated/preview/b79e409078ebfbc02e8615b2ed5938c6-icono-de-mariposa.png?w=360"
@@ -13,9 +13,10 @@
       <div class="flex justify-center gap-6">
         <NuxtLink to="#" class="hover:text-home-text">Instagram</NuxtLink>
         <NuxtLink to="#" class="hover:text-home-text">X</NuxtLink>
+        <NuxtLink to="#" class="hover:text-home-text">Discord</NuxtLink>
       </div>
 
-      <p class="text-right mr-10">
+      <p class="text-right mr-10 text-">
         © 2026 Yesenia Toro. Todos los derechos reservados.
       </p>
     </section>
