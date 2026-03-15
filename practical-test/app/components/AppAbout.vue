062ffb36876d893c8af6bd231d9297a5c376785a
yesenia180405 <yes.ale1804@gmail.com>
2026-03-15 23:44:55 +0100

    fix(app/AppAbout.vue): completion of responsive about section

--- practical-test/app/components/AppAbout.vue
+++ practical-test/app/components/AppAbout.vue
@@ -7,10 +7,10 @@ const skills = [
 ];
 </script>
 <template>
-    <section class="bg-about-bg h-[36vw] flex gap-10 items-center px-96">
+    <section class="bg-about-bg min-h-[72vh] flex flex-col md:flex-row gap-10 items-center md:px-96 w-full ">
         <img src="../assets/img/foto.jpeg" alt="Imagen de perfil"
-            class="m-5 w-[19rem] h-[26rem] rounded rounded-2xl shadow-2xl shadow-[0_0_54px_rgb(147,51,234)]" />
-        <section class="py-40">
+            class="w-[19rem] h-[26rem] rounded rounded-2xl object-cover shadow-2xl shadow-[0_0_54px_rgb(147,51,234)] mt-10" />
+        <section class="md:py-40 mt-5 px-8">
             <h2 class="text-3xl font-semibold pb-7"><span class="text-hero-bg">Uniendo </span>
                 <span class="text-button-hover"> Diseño </span>
                 <span class="text-hero-bg"> y</span>
@@ -24,7 +24,7 @@ const skills = [
 
             <div class="flex gap-3 mt-4">
                 <span v-for="skill in skills" :key="skill.id"
-                    class="border border-gray-400 rounded-lg px-4 py-1 text-sm text-about-text bg-white">
+                    class="border border-gray-400 rounded-lg px-4 py-1 text-sm text-about-text bg-white my-6">
                     {{ skill.text }}
                 </span>
             </div>
