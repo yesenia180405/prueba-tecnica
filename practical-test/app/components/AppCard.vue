a9008ed667d6893c390718e0cf53344d71c165ca
yesenia180405 <yes.ale1804@gmail.com>
2026-03-16 10:31:03 +0100

    feat(app/AppCard): creation of a new component AppCard

---
+++ practical-test/app/components/AppCard.vue
@@ -0,0 +1,18 @@
+<script setup>
+const props = defineProps({
+  img: String,
+  tags: Array,
+  title: String,
+  description: String,
+  link: String,
+})
+</script>
+<template>
+  <section>
+    <img :src="props.img" :alt="props.title" />
+    <AppTags />
+    <h2 aria-label="Título del proyecto">{{ props.title }}</h2>
+    <p>{{ props.description }}</p>
+    <a :href="props.link"> Ver más → </a>
+  </section>
+</template>
