bc32b876e15f14631307379f2f7f7ca39210760d
yesenia180405 <yes.ale1804@gmail.com>
2026-03-19 11:41:07 +0100

    feat(app/assets/skills.json): creation and implementation of skills JSON on AppSkill

--- practical-test/app/components/AppCardSkill.vue
+++ practical-test/app/components/AppCardSkill.vue
@@ -1,8 +1,14 @@
 <script setup>
 const props = defineProps({
-  icono: [Object, Function],
+  icono: String,
   label: String,
   link: String,
+})
+
+const svgContent = ref('')
+onMounted(async () => {
+  const res = await fetch(props.icono)
+  svgContent.value = await res.text()
 })
 </script>
 <template>
@@ -10,9 +16,9 @@ const props = defineProps({
     class="bg-card-bg rounded-2xl aspect-square gap md:w-[12vw] md:h-[19vh] flex flex-col items-center justify-center text-center group hover:bg-purple-300"
   >
     <a :href="link" target="_blank">
-      <component
-        :is="props.icono"
-        class="text-link-text group-hover:text-white h-[5em] w-[5em] mt-6"
+      <div
+        v-html="svgContent"
+        class="text-link-text group-hover:text-white h-[5em] w-[5em] mt-6 [&>svg]:h-full [&>svg]:w-full [&>svg]:fill-current"
       />
       <p class="py-5 font-semibold text-project-text">{{ props.label }}</p>
     </a>
