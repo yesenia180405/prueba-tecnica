f7fc74409033b2d0663418b2b355a474f92add2c
yesenia180405 <yes.ale1804@gmail.com>
2026-03-19 09:34:14 +0100

    fix(app/AppHero.vue): full translation of the hero section

--- practical-test/app/components/AppHero.vue
+++ practical-test/app/components/AppHero.vue
@@ -20,12 +20,13 @@
       class="text-[rgb(199,189,215)] md:text-2xl text-lg my-9 mx-10 md:w-[55vw]"
       aria-label="Descripción"
     >
-      Desarrolladora web apasionada por el frontend y el backend, en constante
-      formación para crear aplicaciones completas, modernas y funcionales que
-      marquen la diferencia.
+      A web developer with a passion for front-end and back-end development,
+      constantly updating my skills to create comprehensive, modern and
+      functional applications that make a difference.
     </p>
+
     <AppButton styles="rounded-3xl p-3 px-10 py-4 mt-4">
-      <a href="#projects">Ver proyectos</a>
+      <a href="#projects">View projects</a>
     </AppButton>
   </section>
 </template>
