917d84155437299bfa0464f1ea920d44b264e8af
yesenia180405 <yes.ale1804@gmail.com>
2026-03-20 13:00:06 +0100

    feat(components/projects/ProjectAbout.vue): create the component ProjectAbout

--- practical-test/app/components/project/ProjectAbout.vue
+++ practical-test/app/components/project/ProjectAbout.vue
@@ -1,1 +1,19 @@
-<template></template>
+<script setup>
+import proyectos from '../../data/projects.json'
+</script>
+<template>
+  <section
+    class="bg-project-bg min-h-[40vw] md:p-20 p-8 pt-20 flex gap-8 justify-center"
+  >
+    <ProjectFilter></ProjectFilter>
+    <AppCard
+      v-for="project in proyectos"
+      :key="project.name"
+      :img="project.img"
+      :tags="project.tags"
+      :title="project.name"
+      :description="project.description"
+      :link="project.link"
+    />
+  </section>
+</template>
