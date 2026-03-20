94efef1781009fa40e260ed67118bcc925bbfd18
yesenia180405 <yes.ale1804@gmail.com>
2026-03-20 14:20:37 +0100

    feat(components/projects/ProjectAbout.vue): complete full responsive

--- practical-test/app/components/project/ProjectHero.vue
+++ practical-test/app/components/project/ProjectHero.vue
@@ -1,12 +1,15 @@
 <script></script>
 <template>
   <section
-    class="bg-about-bg min-h-[50vh] pt-52 flex flex-col gap-10 items-center"
+    class="bg-about-bg min-h-[50vh] pt-40 flex flex-col gap-10 justify-center items-center w-full px-16 pb-10"
   >
-    <h1 class="text-7xl font-bold" aria-label="All projects">
+    <h1
+      class="md:text-7xl text-5xl text-center font-bold"
+      aria-label="All projects"
+    >
       Exploring my <span class="text-project-text">Projects</span>
     </h1>
-    <p class="text-2xl w-[56vw] text-center text-purple-950">
+    <p class="md:text-2xl text-xl w-[56vw] text-center text-purple-950">
       A collection of digital projects where clean design meets solid
       functionality, built with curiosity and attention to detail.
     </p>
