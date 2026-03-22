025561e7627d0713f5d482b75cbd554ca200d12f
yesenia180405 <yes.ale1804@gmail.com>
2026-03-22 15:25:31 +0100

    feat(app/project): create a new page projects

--- practical-test/app/components/project/ProjectFilter.vue
+++ practical-test/app/components/project/ProjectFilter.vue
@@ -2,17 +2,15 @@
 const filters = useFilter()
 </script>
 <template>
-  <section
-    class="flex rounded-lg overflow-hidden justify-center w-full flex-wrap md:text-base text-sm"
-  >
-    <button
-      v-for="tag in filters.allTags"
-      :key="tag"
-      @click="filters.selectedTag = tag"
-      class="bg-purple-200 px-8 py-3 hover:bg-purple-300 font-medium mb-8 transition-all"
-      :class="filters.selectedTag === tag ? 'bg-purple-300' : ''"
-    >
-      {{ tag }}
-    </button>
+  <section class="flex rounded-lg overflow-hidden justify-center w-full flex-wrap md:text-base text-sm">
+
+    <!-- Filtros en mobile -->
+    <div class="flex overflow-x-auto px-4 pb-2 scrollbar-hide ">
+      <button v-for="tag in filters.allTags" :key="tag" @click="filters.selectedTag = tag"
+        class="bg-purple-200 px-8 py-2 lg:py-3 hover:bg-purple-300 font-medium scrollbar-hide transition-all"
+        :class="filters.selectedTag === tag ? 'bg-purple-300' : ''">
+        {{ tag }}
+      </button>
+    </div>
   </section>
 </template>
